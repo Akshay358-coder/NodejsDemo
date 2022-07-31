ed4dd00d06a1a5910cb638f72c1be5a583915bba
Akshay358-coder <52698013+Akshay358-coder@users.noreply.github.com>
2022-07-31 17:50:08 +0530

    Update sonar-project.js

--- Desktop/Project/Project/sonar-project.js
+++ Desktop/Project/Project/sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.127.85.206:9980/',
+  serverUrl: 'http://ec2-43-205-118-155.ap-south-1.compute.amazonaws.com:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
