97b15ae2b4abe4f58cb4671a06b56d33263b1f5a
Akshay358-coder <52698013+Akshay358-coder@users.noreply.github.com>
2022-08-01 16:45:24 +0530

    Update app.js

--- Desktop/Project/Project/app.js
+++ Desktop/Project/Project/app.js
@@ -1,80 +1,19 @@
-var express = require("express"); 
-const path = require('path');
-const cfenv = require('cfenv');
+var http = require('http');
 
-var app  =  express(); 
-var appEnv = cfenv.getAppEnv();
-var url = process.env.url
-app.set('port', (process.env.PORT || 9981))
-app.use(express.static(__dirname + '/images'))
+var server = http.createServer(function(request, response) {
 
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello, Welcome to DevOps CI CD");
 
-/*
-app.get("/getCall", function(req,res){ 
-console.log("GET Method caled");
-console.log(__dirname);
-
-res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+process.env.LOGNAME);
-
-}).listen(9009);
-console.log(__dirname+"/images/mithunlogo.jpg");
-*/
-app.get('/globant', function(request, response) {
-    //response.send("<h2><center>Welcome to Node JS app</h2>");
-    response.write("<h2><center><u>Node JS  Application </u></center></h2>");
-	
-    response.write("<h2><center>Welcome to  Globant.Don't forget to smile<center></h2>" );
-    response.end();
-    
-  })
- 
-
-//app.get("/html", function(req,res){
-app.get("/html", function(req,res){
-    res.set("Content-Type","text/html");
-    //res.contentType("html") ; 
-    res.write("<h2>Welcome Docker</h2>");
-    res.write("<h2>/html call</h2>");
-    //must end 
-    res.end();
-    
-    });
-    app.get("/jsonData", function(req,res){
-        res.type('json');
-        //res.type('application/json');
-        //res.json({'name': 'Mithun Reddy L'});
-        res.send({
-		'name': 'Akshay Wankhede'
-		'technology': 'DevOps'
-	            });
-        
-        });
-app.get("/queryparam", function(req,res){
-//res.send(req.query);
-res.send(req.query.key + ": " + req.query.name);
 });
 
-app.get("/status-code-404", function(req, res) {
-    //set content-type to application/json
-    //res.sendStatus(404);
-      res.status(404).send('Sorry, we cannot find that!');
-})
+var port = 3000;
+server.listen(port);
+module.exports = server
+console.log("Server is available on Port:", port);
 
-app.get("/status-code-500", function(req, res) {
-    //set content-type to application/json
-    //res.sendStatus(500);
-   res.status(500).send('Internal Server Error – custom message');
-})
 
-app.get('/redirect', function(req, res) {
-    //Send status 300
-        res.redirect('http://globant.com');
-    });
-    
- 
-    app.listen(app.get('port'), function() {
-        console.log("Node JS app is running at http://localhost:" + app.get('port') +"/globant");
-      })
+
     
 
 
